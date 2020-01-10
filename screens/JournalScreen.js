b7d6ddbfb96f885d15ffdc3ce9c0510e1db5e408
JosephWyman328 <Jwyman@student.framingham.edu>
2020-01-10 16:30:29 -0500

    Add title to journal screen

--- screens/JournalScreen.js
+++ screens/JournalScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View , Dimensions} from 'react-native';
 import { Slider } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import colors from '../constants/colors';
 
 
 
@@ -9,20 +10,19 @@ function JournalScreen() {
     const [happynessValue, sethappynessValue] = useState(3)
     let [faceEmotion, setFaceEmotion] = useState('neutral')
     const [face, setFace] = useState('emoticon-neutral')
+    const faceEmotions = ['terrible', 'bad', 'neutral', 'good', 'excellent']
+    const faces = ['emoticon-dead','emoticon-sad', 'emoticon-neutral', 'emoticon-happy','emoticon-excited']
 
     const returnFace = () => {
-        const faceEmotions = ['terrible', 'bad', 'neutral', 'good', 'excellent']
-        const faces = ['emoticon-dead','emoticon-sad', 'emoticon-neutral', 'emoticon-happy','emoticon-excited']
+
 
         setFaceEmotion(faceEmotions[happynessValue-1])
         const face = faces[happynessValue-1]
         setFace(face)
-        console.log(face)
 
     }
 
     useEffect(() => {
-        console.log('changes')
         returnFace()
     },[happynessValue])
 
@@ -32,8 +32,10 @@ function JournalScreen() {
             < View style={styles.container} >
             </View >
             <View>
+            <Text>How are you feeling?</Text>
+
                 <Text>{faceEmotion} </Text>
-                <MaterialCommunityIcons size={70} name={face} title='play' />
+                <MaterialCommunityIcons size={150} color={colors.lightSecondary} name={face} title='play' />
             </View>
 
 
